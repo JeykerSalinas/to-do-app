73e79e3f3cab2b7e1bbf7b14bda2ca07f3140a25
JeykerSalinas <66428501+JeykerSalinas@users.noreply.github.com>
2022-03-09 22:19:11 -0300

    First commit, this app takes an input and render to a list

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1>Todo List App</h1>
+        </div>
+      </div>
+      <div class="row">
+        <h2 class="col-12">Tarea</h2>
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder=""
+            aria-label="Example text with button addon"
+            aria-describedby="button-addon1"
+            v-model="myTask"
+          />
+          <button
+            class="btn btn-outline-secondary"
+            type="button"
+            id="button-addon1"
+            @click="addTask"
+          >
+            Crear
+          </button>
+        </div>
+
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item my-2" v-for="(item, i) in items" :key="i">
+            {{ item }}
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      myTask: "",
+      items: [],
+    };
+  },
+  methods: {
+    addTask() {
+      this.myTask === ""
+        ? alert("Por favor ingresar tarea")
+        : this.items.push(this.myTask);
+    },
   },
 };
 </script>
